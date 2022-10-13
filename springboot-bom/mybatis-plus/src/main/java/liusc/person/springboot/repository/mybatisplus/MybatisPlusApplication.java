bde3c31d89ba1f6071b8970c1110f2be26f171ce
刘师臣 <1759292916@qq.com>
2022-10-13 14:11:45 +0800

    【add】 checkstyle 集成

--- springboot-bom/mybatis-plus/src/main/java/liusc/person/springboot/repository/mybatisplus/MybatisPlusApplication.java
+++ springboot-bom/mybatis-plus/src/main/java/liusc/person/springboot/repository/mybatisplus/MybatisPlusApplication.java
@@ -3,6 +3,11 @@ package liusc.person.springboot.repository.mybatisplus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * mybatis-plus 启动类
+ * @author LiuShiChen
+ * @date 2022/10/13 14:08:53
+ **/
 @SpringBootApplication
 public class MybatisPlusApplication {
 
